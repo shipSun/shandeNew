85b9e17860a625a441565e5eaed305c2e8e75dd3
ship <543999860@qq.com>
2022-07-19 18:23:17 +0800

    init

--- src/Sign.php
+++ src/Sign.php
@@ -8,12 +8,46 @@
 namespace ShanDe;
 
 class Sign{
-    public static function encrypt($data,$type){
-
+    public static function encrypt($data,$key,$type){
+        $sign = static::getSignContent($data).'&key='.$key;
+        if($type == 'md5'){
+            $sign = md5($sign);
+        }
+        return strtoupper($sign);
     }
+    protected static function getSignContent($params) {
+        ksort($params);
 
-    public static function vertify($data, $sign, $type){
-        if(static::encrypt($data,$type)==$sign){
+        $stringToBeSigned = "";
+        $i = 0;
+        foreach ($params as $k => $v) {
+            if (false === static::checkEmpty($v) && "@" != substr($v, 0, 1)) {
+
+                if ($i == 0) {
+                    $stringToBeSigned .= "$k" . "=" . "$v";
+                } else {
+                    $stringToBeSigned .= "&" . "$k" . "=" . "$v";
+                }
+                $i++;
+            }
+        }
+
+        unset ($k, $v);
+        return $stringToBeSigned;
+    }
+    protected static function checkEmpty($value)
+    {
+        if (!isset($value))
+            return true;
+        if ($value === null)
+            return true;
+        if (trim($value) === "")
+            return true;
+
+        return false;
+    }
+    public static function vertify($data, $key, $sign, $type){
+        if(static::encrypt($data, $key, $type)==$sign){
             return true;
         }
         return false;
